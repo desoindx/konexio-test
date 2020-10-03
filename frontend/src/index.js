7d275b543cbef227fce993ee43a5e2608c54534a
Xavierd <xavier.desoindre@hotmail.fr>
2020-10-03 16:07:39 +0200

    Init bootstrap form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Register from './register';
 
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
--- frontend/src/register.js
+++ frontend/src/register.js
@@ -1,22 +1,40 @@
-import { basename } from 'path';
 import React from 'react';
-import agent from './agent';
+
+import { Button, Form } from 'react-bootstrap';
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: 'loading' };
+    this.state = {};
   }
 
-  componentDidMount() {
-    agent.Register.helloWorld().then(x => { this.setState({ text: x.text }); });
-  }
+    handleSubmit = event => {
+      const form = event.currentTarget;
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
 
-  render() {
-    return (
-      <div>{this.state.text}</div>
-    );
-  }
+      // console.log('submit', event.target.element.firstName.value);
+      this.setState({ validated: true });
+      event.preventDefault();
+    };
+
+    render() {
+      return (
+        <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+          <Form.Group controlId="firstName">
+            <Form.Control required type="text" placeholder="First name..." />
+            <Form.Control.Feedback type="invalid">
+              Your first name is mandatory to register.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      );
+    }
 }
 
 export default Register;
